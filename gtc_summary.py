ea59a095b3cfb6f28b821664aa28b379a6978d28
Site Wang <sitewang@Sites-MacBook-Pro.local>
2024-04-08 22:15:32 -0700

    basic change

--- gtc_summary.py
+++ gtc_summary.py
@@ -28,8 +28,40 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
 KEYNOTE_PERSIST_DIR = ".keynote_storage"
 NOTES_PERSIST_DIR = ".notes_storage"
+NOTES_DAT_DIR = "summarized_notes"
+
+
+SYSTEM_PROMPT = """
+    You are an helper to assis Site Wang who just attended GTC 2024 to answer questions related to the conference. 
+    Assume that all questions are based on Site's experience which will be provided as context. 
+    Keep your answers relatable and friendly and based on facts provided in the context – do not hallucinate.
+    Be patient with technical terms and always assume your audience is not familiar with the technical jargon.
+"""
+
+
+def collect_md_files(folder_path):
+    """
+    Recursively collects all .md files from a specified folder.
+
+    Parameters:
+    - folder_path (str): The path to the folder from which to collect .md files.
+
+    Returns:
+    - list[str]: A list of full paths to the .md files found within the folder and its subfolders.
+    """
+    md_files = []
+
+    # Walk through the directory
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith(".md"):
+                full_path = os.path.join(root, file)
+                md_files.append(full_path)
+
+    return md_files
 
 
 def stream_data(response):
@@ -37,6 +69,28 @@ def stream_data(response):
         yield word + " "
         time.sleep(0.02)
 
+
+def qa_chat_engine(doc_dir, persist_dir):
+    
+    if not os.path.exists(persist_dir):
+        documents = SimpleDirectoryReader(doc_dir).load_data()  # load the documents and create the index
+        if st.session_state.llm_name == "OpenAI":
+            service_context = ServiceContext.from_defaults(llm=st.session_state.llm)
+            index = VectorStoreIndex.from_documents(documents, service_context=service_context, similarity_top_k=5)    
+        else:
+            index = VectorStoreIndex.from_documents(documents)
+        index.storage_context.persist(persist_dir=persist_dir)
+    else:
+        # load the existing index
+        storage_context = StorageContext.from_defaults(persist_dir=persist_dir)
+        index = load_index_from_storage(storage_context, similarity_top_k=5)
+
+    if st.session_state.llm_name != "OpenAI":  # additional steps for ollama - which I think I can remove
+        Settings.embed_model = st.session_state.embeddings
+        Settings.llm = Ollama(model="mistral", request_timeout=30.0)
+
+    if 'chat_engine' not in st.session_state or st.session_state.chat_engine is None:
+        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question")
 
 def keynote_qa():
 
@@ -48,34 +102,8 @@ def keynote_qa():
 
     if 'keynote_messages' not in st.session_state:
         reset_keynote_messages()
-
-    # check if storage already exists
-    if not os.path.exists(KEYNOTE_PERSIST_DIR):
-        # load the documents and create the index
-        documents = SimpleDirectoryReader("keynote").load_data()
-        # index = VectorStoreIndex.from_documents(documents)
-        service_context = ServiceContext.from_defaults(
-            llm=OpenAI(
-                model="gpt-3.5-turbo", 
-                temperature=0.5, 
-                system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."
-                )
-            )
-        index = VectorStoreIndex.from_documents(documents, service_context=service_context)
-        index.storage_context.persist(persist_dir=KEYNOTE_PERSIST_DIR)
-    else:
-        # load the existing index
-        storage_context = StorageContext.from_defaults(persist_dir=KEYNOTE_PERSIST_DIR)
-        index = load_index_from_storage(storage_context)
-
-    # embedding model
-    # Settings.embed_model = st.session_state.embeddings
-
-    # ollama
-    # Settings.llm = Ollama(model="mistral", request_timeout=30.0)
-
-    if 'chat_engine' not in st.session_state or st.session_state.chat_engine is None:
-        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question")
+    
+    qa_chat_engine("keynote", KEYNOTE_PERSIST_DIR)
 
     for message in st.session_state.keynote_messages: # Display the prior chat messages
         with st.chat_message(message["role"]):
@@ -91,6 +119,7 @@ def keynote_qa():
         with st.chat_message("agent", avatar="🤖"):
             st.write_stream(stream_data(response.response))
         st.rerun()
+
     st.button("Start New Chat 🧹", on_click=reset_keynote_messages, use_container_width=True)
 
 
@@ -105,36 +134,7 @@ def ama_qa():
     if 'ama_messages' not in st.session_state:
         reset_ama_messages()
 
-    # check if storage already exists
-    if not os.path.exists(NOTES_PERSIST_DIR):
-        # load the documents and create the index
-        documents = SimpleDirectoryReader("keynote").load_data()
-        # index = VectorStoreIndex.from_documents(documents)
-        service_context = ServiceContext.from_defaults(
-            llm=OpenAI(
-                model="gpt-3.5-turbo", 
-                temperature=0.5, 
-                system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."
-                )
-            )
-        index = VectorStoreIndex.from_documents(documents, service_context=service_context)
-        index.storage_context.persist(persist_dir=KEYNOTE_PERSIST_DIR)
-    else:
-        # load the existing index
-        storage_context = StorageContext.from_defaults(persist_dir=NOTES_PERSIST_DIR)
-        index = load_index_from_storage(storage_context)
-
-    if 'embeddings' not in st.session_state:
-        st.session_state.embeddings = resolve_embed_model("local:BAAI/bge-small-en-v1.5")
-
-    # embedding model
-    # Settings.embed_model = st.session_state.embeddings
-
-    # ollama
-    # Settings.llm = Ollama(model="mistral", request_timeout=30.0)
-
-    if 'ama_chat_engine' not in st.session_state or st.session_state.ama_chat_engine is None:
-        st.session_state.ama_chat_engine = index.as_chat_engine(chat_mode="condense_question")
+    qa_chat_engine("summarized_notes", NOTES_PERSIST_DIR)
 
     for message in st.session_state.ama_messages: # Display the prior chat messages
         with st.chat_message(message["role"]):
@@ -150,7 +150,9 @@ def ama_qa():
         with st.chat_message("agent", avatar="🤖"):
             st.write_stream(stream_data(response.response))
         st.rerun()
+
     st.button("Start New Chat 🧹", key='ama_clean', on_click=reset_ama_messages, use_container_width=True)
+
 
 def talk_show():
 
@@ -211,6 +213,15 @@ def company_show():
             st.write_stream(stream_data(answer_str))
 
 
+def show_summarized_notes():
+
+    md_files = collect_md_files('summarized_notes')
+    selected_md_file = st.selectbox("Select a summarized transcript to view", md_files, index=None)
+    if selected_md_file:
+        with open(str(selected_md_file), "r") as file:
+            st.markdown(file.read())
+
+
 def main():
 
     st.title("GTC 2024 | A Summary")
@@ -218,8 +229,15 @@ def main():
     if 'embeddings' not in st.session_state:
         st.session_state.embeddings = resolve_embed_model("local:BAAI/bge-small-en-v1.5")
 
+    st.session_state.llm_name = st.sidebar.selectbox("LLM Model", ["OpenAI", "Ollama"], index=0)
+    
+    if st.session_state.llm_name == "OpenAI":
+        st.session_state.llm = OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt=SYSTEM_PROMPT)
+    else:
+        st.session_state.llm = Ollama(model="mistral", request_timeout=30.0)
+
     tab_intro, tab_keynote, tab_ama, tab_talks, tab_companies = st.tabs(
-        ["👋 Welcome!", "🏆 The Keynote", "📕 Ask My Notes", "🎙️ Technical Talks", "🏢 Companies", ]
+        ["👋 Welcome!", "🏆 The Keynote", "📕 My Notes", "🎙️ Technical Talks", "🏢 Companies", ]
     )
 
     with tab_intro:
@@ -237,17 +255,19 @@ def main():
         st.subheader("The Summary")
         notes_summary()
         notes_pictures()
+        st.write('---')
+        show_summarized_notes()
+        st.write('---')
         ama_qa()
 
     with tab_talks:
         st.info("Search for Collected Technical Talks and Read/Downlaod PDFs")
         talk_show()
-        # Try to see if there is a way to summarize the technical keynotes - with abstract information also extracted
 
     with tab_companies:
         st.subheader("The Companies")
-        company_show()
-        # https://www.nvidia.com/gtc/sponsors/#/
+        company_show() # https://www.nvidia.com/gtc/sponsors/#/
+        
 
 
 
---
+++ summarized_notes/How To Write A CUDA Program: The Ninja Edition [S62401].md
@@ -0,0 +1,52 @@
+# How To Write A CUDA Program: The Ninja Edition [S62401] - GTC 2024
+
+## Introduction
+- Importance of understanding CUDA for parallel computing
+- Overview of the CUDA programming model and its capability for massively parallel execution
+
+## Understanding CUDA's Parallelism
+- Discussion on model parallelism and its significance in CUDA programming
+- Explanation of task and data parallelism within the CUDA context
+
+## Data Parallelism in CUDA
+- Introduction to the concept of data parallelism and its ease of understanding
+- Explanation of how data parallelism is applied in CUDA through an example involving task execution times and GPU utilization
+
+## CUDA Execution Model
+- Explanation of CUDA's execution model involving blocks, threads, and grids
+- Discussion on the intrinsic nature of CUDA being both task and data parallel
+
+## Wave Quantization and Its Impact
+- Detailed discussion on wave quantization and its effects on program performance
+- Example provided to illustrate how wave quantization can lead to underutilization of GPU resources
+
+## Task Parallelism in CUDA
+- Introduction to task parallelism and its contrast with data parallelism
+- Explanation of how task parallelism is utilized in CUDA for optimizing program execution
+
+## Memory Management in CUDA
+- Discussion on the importance of memory management in CUDA for optimal performance
+- Techniques for managing memory more efficiently in CUDA programs
+
+## Task Parallelism Strategies
+- Explanation of various strategies to implement task parallelism in CUDA
+- Discussion on the challenges and considerations in designing task-parallel CUDA programs
+
+## Memory Efficiency and Caching
+- Importance of memory efficiency and caching for CUDA program performance
+- Strategies for optimizing memory usage and leveraging cache effectively in CUDA
+
+## Advanced Parallelism Techniques
+- Exploration of more advanced parallelism techniques in CUDA, including dynamic task creation and dependency management
+- Discussion on the challenges and potential performance gains from these advanced techniques
+
+## Performance Optimization
+- Tips and strategies for optimizing CUDA program performance, including memory access patterns and execution configuration
+- Real-world examples to illustrate how these optimizations can lead to significant performance improvements
+
+## Conclusion and Future Directions
+- Recap of the key points discussed in the talk
+- Insight into future trends and developments in CUDA programming and parallel computing
+
+## Q&A Session
+- Open floor for audience questions and further discussion on CUDA programming and optimization techniques
